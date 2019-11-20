00bc91bf8cc0d4df3e90cb3594ed150288beda9f
litoss <58005571+litoss@users.noreply.github.com>
2019-11-20 19:40:34 +0100

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,34 @@
+var content = [
+  { id:"none", name:"Nessuna"},
+  { id:"nat", name:"Natura"},
+  { id:"art", name:"Arte"},
+  { id:"his", name:"Storia"},
+  { id:"flk", name:"Folklore"},
+  { id:"mod", name:"Cultura moderna"},
+  { id:"rel", name:"Religione"},
+  { id:"cui", name:"Cucina e Drink"},
+  { id:"spo", name:"Sport"},
+  { id:"mus", name:"Musica"},
+  { id:"mov", name:"Film"},
+  { id:"fas", name:"Moda"},
+  { id:"shp", name:"Shopping"},
+  { id:"tec", name:"Tecnologia"},
+  { id:"pop", name:"Cult. pop. e gossip"},
+  { id:"prs", name:"Nessuna"}, //nome errato
+  { id:"none", name:"Esperienze personali"},
+  { id:"oth", name:"Altro"}
+];
+
+var audience = [
+  { id:"gen", name:"Pubblico Generico"},
+  { id:"pre", name:"Pre-Scuola"},
+  { id:"elm", name:"Scuola Primaria"},
+  { id:"mid", name:"Scuola Media"},
+  { id:"scl", name:"Specialisti del Settore"},
+];
+
+var classifiche = [
+  { icon: "queue_music", name: "Clip"},
+  { icon: "person", name: "Vloggers"},
+  { icon: "navigation", name: "Percorsi"}
+];
