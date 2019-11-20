5c5d1470039d19a1816e3687b70c430e8f5a9ffd
litoss <58005571+litoss@users.noreply.github.com>
2019-11-20 19:36:03 +0100

    Add files via upload

--- model/Checkbox.js
+++ model/Checkbox.js
@@ -1,11 +1,30 @@
 class Checkbox extends mdc.checkbox.MDCCheckbox{
     constructor(id) {
-        var item = document.createElement('div');
-        item.className = "mdc-checkbox";
-        item.innerHTML =
-        '<input type="checkbox" id="' + id + '" class="mdc-checkbox__native-control"/>' +
-        '<div class="mdc-checkbox__background"></div>';
+        var div_checkbox = document.createElement('div');
+        div_checkbox.className = "mdc-checkbox";
 
-        super(item);
+        var input = document.createElement('input');
+        input.className = "mdc-checkbox__native-control";
+        input.setAttribute('type','checkbox');
+        input.setAttribute('id',id);
+
+        var div_background = document.createElement('div');
+        div_background.className = "mdc-checkbox__background";
+
+        var svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+        svg.setAttribute('class', 'mdc-checkbox__checkmark');
+        svg.setAttribute('viewBox', '0 0 24 24');
+
+        var path = document.createElementNS('http://www.w3.org/2000/svg',"path");
+        path.setAttribute('class', 'mdc-checkbox__checkmark-path');
+        path.setAttribute( 'd', 'M1.73,12.91 8.1,19.28 22.79,4.59');
+        path.setAttribute( 'fill', 'none');
+
+        svg.appendChild(path);
+        div_background.appendChild(svg);
+        div_checkbox.appendChild(input);
+        div_checkbox.appendChild(div_background);
+        
+        super(div_checkbox);
     }
 }
