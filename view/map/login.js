f2074ced0d9604d883c3c4b40358806a80ea5ede
litoss <carlos.caramaschi@gmail.com>
2019-11-23 07:17:49 +0100

    Modifica al pulsante login

--- view/map/login.js
+++ view/map/login.js
@@ -1,40 +1,21 @@
 class Login{
   constructor(){
-
     var login = document.createElement('div');
-    login.className = "toolbar mdc-menu-surface--anchor";
-    login.setAttribute('id','toolbar');
-    var button = new IconButton('account_circle');
-    var menu = new Menus('test');
-
-    button.listen('click', () => {
-      menu.open = true;
-    })
-
-    login.appendChild(button.root_);
-    login.appendChild(menu.root_);
-
+    gapi.signin2.render(login, {
+      'scope': 'profile email',
+      'width': 150,
+      'height': 48,
+      'theme': 'dark',
+      'onsuccess': this.onSuccess,
+      'onfailure': this.onFailure
+    });
     return login;
   }
-}
-/*gapi.load('auth2', function(){
-  auth2 = gapi.auth2.init({
-    client_id: '588726918570-3tfcmo8nh5al0mupr29rsjmlop8jm9ce.apps.googleusercontent.com',
-    cookiepolicy: 'single_host_origin',
-  });
 
-  auth2.attachClickHandler(login.root_, {},
-      function(googleUser) {
-        console.log(googleUser);
-      }, function(error) {
-        alert(JSON.stringify(error, undefined, 2));
-      });
-});
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
+  onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
+  onFailure(error) {
+    console.log(error);
+  }
 }
-*/
