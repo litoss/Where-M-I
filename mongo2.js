552af7d1ab24eb2ca63e13288dd0f453e583bc83
SF-Fox <45967560+SF-Fox@users.noreply.github.com>
2019-12-13 15:15:08 +0100

    Add files via upload

---
+++ mongo2.js
@@ -0,0 +1,637 @@
+/*
+Promise is an object used for deferred and asynchronous computations. It represents an operation that has not completed yet, but is expected in the future.
+
+asyncFunc()
+  .then(value => { * success * })
+  .catch(error => { * failure * })
+  .finally( => { * cleanup *};
+The then() method always returns a Promise, which enables us to chain method calls.
+
+Note: the MongoClient's connect returns a promise if no callback is passed.
+We can also use async/await syntax to work with promises.
+
+*/
+
+const mongo = require('mongodb');
+
+const MongoClient = mongo.MongoClient;
+const ObjectID = mongo.ObjectID; //serve per poter passare i parametri in name e price dentro ObjectID
+const url = 'mongodb://localhost:27017';
+//const url = 'mongodb://site181927:Aeho3ael@mongo_site181927';
+
+
+exports.add_one = async (req) => {
+    
+    var m_rating = 0; // alla creazione di un nuovo luogo settiamo la media a 0 dato non ci sono ancora recensioni
+
+    try {
+        let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+        const db = client.db("webdb");
+
+        /*  anche se non viene fatto un controllo di quello che viene passato dal client nel json quando si aggiunge nel
+        DB viene creata la struttura e i parametri che non vengono inseriti nel json sono semplicemente settati a undefined ed
+        eventualmente modificati nel futuro. */
+
+        let doc = {_id: new ObjectID(), 
+            OLC: req.body.OLC,                  //codice location
+            user: req.body.user,                //nome user che crea il luogo
+            name: req.body.name,                //nome del posto
+            category: req.body.category,        // categoria del luogo(es. pizzeria, museo)
+            media_rating: m_rating,             //media rating a zero alla creazione del luogo
+            opening: req.body.opening,          // orari di apertura del luogo          
+            description: req.body.description   // descrizione del luogo
+         };
+        
+        let ret = await db.collection('place').insertOne(doc);
+        console.log(doc) // display the inserted information
+        client.close();
+        return ret;
+
+        } 
+    catch (err) {
+        throw err;
+    }
+}
+
+
+exports.add_review = async (req) => {
+   
+    console.log("richiesta di aggiunta review:x " + JSON.stringify(req.body));
+    console.log('\n');
+
+    try{
+        let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+        const db = client.db("webdb");
+
+        //check if exist the review of the user
+        var olc = req.body.OLC;
+        var utente = req.body.user;
+
+        var query = {$and: [{OLC:{$regex:olc}} , {user:{$regex:utente}} ] };
+
+        var exist = await db.collection('review').find(query).count() > 0; // aggiungendo il .count() > 0 ritorna true se e' presente nel database else false 
+
+        //if the OLC of this user is not in the DB create it
+
+        console.log("review user exist: " + exist);
+
+        if(exist == false){
+
+            
+            var v_tag;
+            if (req.body.visit_tag){
+
+                v_tag = req.body.visit_tag;
+            }
+            else{
+                v_tag = false;
+            }
+
+            let doc = {_id: new ObjectID(), 
+                        OLC: req.body.OLC,
+                        user: req.body.user,
+                        rating_audio: req.body.rating_audio,
+                        rating_place: req.body.rating_place,
+                        visit_tag: v_tag, 
+                        comment: req.body.comment
+                    }
+            let ret_new = await db.collection('review').insertOne(doc);
+            console.log("review added");
+            console.log(ret_new.result);
+
+        client.close();
+        
+        if (req.body.rating_place || req.body.rating_audio){
+
+            up_star(req); //with the OLC we update the media of rating of the place
+        }
+
+        return ret_new;
+
+        }
+        else{ //if the OLC for the user is already inserted
+
+ 
+            var object_body = {}; //create the object with the values to update
+
+            if (req.body.rating_audio && req.body.rating_audio != '' ){
+                object.rating_audio = req.body.rating_audio
+                
+            }
+            if (req.body.rating_place && req.body.rating_place != '' ){
+                object.rating_place = req.body.rating_place;
+
+            }
+            if (req.body.visit_tag && req.body.visit_tag != '' ){
+               object.visit_tag = req.body.visit_tag;
+
+            }
+            if (req.body.comment && req.body.comment != '' ){
+                object.comment = req.body.comment;
+            }
+
+            var new_values = {$set: object_body}; 
+            var ret_update = await db.collection('review').updateOne(query, new_values); //update with the parameter that are passed trought the body
+
+            client.close();
+
+            
+            return ret_update;
+            
+        }
+
+    }
+    catch(err){
+        console.log(err);
+        throw err;
+    }
+
+    
+
+/* QUANDO SI FA AGGIUNTA DI REVIEW BISOGNA FARE L'UPDATE
+DELLA MEDIA DELLE RECENZIONI NELLA COLLEZIONE PLACE  */
+
+}
+
+
+
+//exports.find = async(olc, utente, nome, categoria, media_rating, orario) => { //ritorna il documento ricercato
+exports.find = async(req) => { //ritorna il documento ricercato
+
+    try{
+        let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+        const db = client.db("webdb");
+        
+        /*
+        inserendo la stringa append davanti al carattere ricercato significa che il $regret (che serve a ricercare il nome anche avendo il nome parziale)
+        ricerchiamo il nome parziale ma deve essere nell-ordine che lo scriviamo, senza lui se cercassimo la lettera S troverebbe anche ad esempio la parola
+        test anche se la S è al centro della parola, con append verrebbe fuori solo la parola Sam che ha la S davanti.
+        */
+
+        var append = '^';
+        var expression = [];
+       
+
+        if (req.body.OLC && req.body.OLC != '' ){
+            var olc = append.concat(req.body.OLC);
+            expression.push({OLC:{$regex:olc}});
+        }
+        if (req.body.user && req.body.user != ''){
+            var utente = append.concat(req.body.user);
+            expression.push({user:{$regex:utente}});
+        }
+        if (req.body.name && req.body.name != ''){
+            var nome = append.concat(req.body.name);
+            expression.push({name:{$regex:nome}});
+        }
+        if (req.body.category){
+            var categoria = append.concat(req.body.category);
+            expression.push({category:{$regex:categoria}});
+        }
+         if (req.body.media_rating){
+            var m_rating = append.concat(req.body.media_rating);
+            expression.push({media_rating:{$regex:m_rating}});
+        }
+        if (req.body.opening){
+            var apertura = append.concat(req.body.opening);
+            expression.push({opening:{$regex:apertura}});
+        } 
+
+        var query;
+            if(expression.length >  1){query = {$and:expression};}
+            if(expression.length == 1){query = expression[0]}
+            
+        var items = await db.collection('place').find(query).project({_id:0}).toArray();
+
+        client.close();
+
+        return items;
+
+    }
+    catch(err){
+        return err;
+
+    }
+}
+
+
+exports.exist_one = async(olc) => { //ritorna true se il codice luogo esiste nella collezione place
+
+    try{
+        let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+        const db = client.db("webdb");
+        var query = {OLC : olc}
+
+        var items = await db.collection('place').find(query).count() > 0; // aggiungendo il .count() > 0 ritorna true se e' presente nel database else false 
+        client.close();
+
+        return items;
+
+    }
+    catch(err){
+        return err;
+
+    }
+}
+
+exports.update_one = async(/* DA INSERIRE I VALORI DELLLA QUERY CHE VOGLIAMO CAMBIARE, */new_values) => {
+
+    //per UPDATE.ONE olc e user sono standard e arrivano per forza, if su i 2 rating e visit e comment e semplicemente sovrascrivo
+
+
+    OLC:
+    user:
+
+
+    try{
+        let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+        const db = client.db("webdb");
+        
+        var query = {  /* DA DECIDERE I VALORI DELLA QUERY DA PASSARE NELLA FUNZIONE, COME RICERCARE I PARAMETRI DA CAMBIARE */ }; 
+
+        
+        var items = await db.collection('review').updateOne(query, new_values);
+
+        client.close();
+
+    }
+    catch(err){
+        return err;
+
+    }
+
+}
+
+
+exports.showdb_place = async () => {
+
+    try{
+        let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+        const db = client.db("webdb");
+        let items = await db.collection('place').find().project({_id:0}).toArray();
+
+        client.close();
+        return items;
+
+        }
+
+    catch (err){
+
+        throw err;
+    }
+}
+
+exports.showdb_review = async () => {
+
+    try{
+        let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+        const db = client.db("webdb");
+       // let items = await db.collection('review').find().project({_id:0}).toArray();
+       let items = await db.collection('review').find().toArray();
+        client.close();
+        return items;
+
+        }
+
+    catch (err){
+
+        throw err;
+    }
+}
+
+exports.clear_place = async() => {
+
+    try{
+        let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+        const db = client.db("webdb");
+        let items = await db.collection('place').drop();
+
+        client.close();
+        return items;
+
+        }
+
+    catch (err){
+
+        throw err;
+    }
+}
+
+ exports.clear_review = async() => {
+
+    try{
+        let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+        const db = client.db("webdb");
+        let items = await db.collection('review').drop();
+
+        client.close();
+        return items;
+
+        }
+        
+    catch (err){
+
+        throw err;
+    }
+}
+ 
+up_star = async(req) => {
+
+    
+    try{
+
+        
+        console.log('\n');
+        
+        let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+        const db = client.db("webdb");
+
+        var query = {OLC : req.body.OLC};
+
+        if(req.body.rating_place){
+            var star = await db.collection('review').aggregate([{$match:query}, {"$group":{"_id":null, rating_place:{"$avg":"$rating_place"}}}]).toArray(); //rating_place deve essere un valore numerico
+        
+            var object = {};
+            object.media_rating = star[0].rating_place
+            var new_values = {$set : object};
+
+            var rate_update_place = await db.collection('place').updateOne(query, new_values); //update with the parameter that are passed trought the body
+
+            console.log(JSON.stringify("rating_place totale aggiornato: " + star[0].rating_place));
+            console.log(rate_update_place.result);
+        }
+
+        if(req.body.rating_audio){
+            var star1 = await db.collection('review').aggregate([{$match:query}, {"$group":{"_id":null, rating_audio:{"$avg":"$rating_audio"}}}]).toArray(); //rating_place deve essere un valore numerico
+      
+            var object1 = {};
+            object1.media_rating = star[0].rating_audio
+            var new_values1 = {$set : object1};
+    
+            var rate_update_audio = await db.collection('place').updateOne(query, new_values1); //update with the parameter that are passed trought the body
+    
+            console.log(JSON.stringify("rating_place totale aggiornato: " + star1[0].rating_audio));
+            console.log(rate_update_audio.result);
+        }
+        
+
+        client.close();
+
+        return ("rate place update: " + rate_update_place.result + "   rate audio update: " + rate_update_audio.result);
+
+        
+    }
+    catch(err){
+        return err;
+    }      
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+exports.showdb = function(ret){
+
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  }, (err, client) => {
+
+        const db = client.db("webdb");
+
+        db.collection('cars').find().toArray((err,items) => {
+            console.log(items);
+            ret(items);
+        })
+
+    })
+}
+*/
+
+function list_coll()
+{
+
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  }, (err, client) => 
+    {
+
+        if (err) throw err;
+
+        const db = client.db("webdb");
+
+        db.listCollections().toArray().then((docs) => 
+        {
+            console.log('Available collections:');
+            docs.forEach((doc, idx, array) => { console.log(doc.name) });
+        })
+   
+        .catch((err) => 
+        {
+          console.log(err);
+        })
+    
+        .finally(() => 
+        {
+            client.close();
+        });
+
+    });
+}
+
+function remove_one(coll , nome)
+{
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+
+        if (err) throw err;
+    
+        const db = client.db("wedb");
+    
+        let query = { name: nome , price: 10};
+    
+        db.collection(coll).deleteOne(query).then((result) => {
+    
+            console.log('Car deleted');
+            console.log(result);
+        }).catch((err) => {
+    
+            console.log(err);
+        }).finally(() => {
+    
+            client.close();
+        });
+    });
+}
+
+function update_one(coll, nome, prezzo)
+{
+
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) =>   
+    {
+
+        if (err) throw err;
+
+        const db = client.db("webdb");
+
+        let filQuery = { name: nome };
+        let updateQuery = { $set: { "price": prezzo }};
+
+        db.collection(coll).updateOne(filQuery, updateQuery).then(result => 
+            {
+                console.log('Car updated');
+                console.log(result);
+
+        })
+        
+        .catch((err) => 
+        {
+            console.log(err);
+        })
+        
+        .finally(() => 
+        {
+            client.close();
+        });
+    });
+}
+
+
+function find_one(coll, nome) //return a document
+{
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => 
+    {
+
+        if (err) throw err;
+    
+        const db = client.db("webdb");
+    
+        let collection = db.collection(coll);
+        let query = { name: nome }
+    
+        collection.findOne(query).then(doc => 
+        {
+            console.log(doc);
+        })
+        
+        .catch((err) => 
+        {
+            console.log(err);
+        })
+        
+        .finally(() => 
+        { 
+            client.close();
+        });
+    });
+}
+
+function find(coll, nome, pippo) //return a collection
+{
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => 
+    {
+
+        if (err) throw err;
+    
+        const db = client.db("webdb");
+
+
+        db.collection(coll).find({}).project({_id: 0}).toArray().then((docs) => //.project serve ad escludere l'id dall'output
+        {
+            console.log(docs);
+            
+
+        })
+        
+        .catch((err) => 
+        {
+            console.log(err);
+        })
+        
+        .finally(() => 
+        { 
+            client.close();
+        });
+    });
+}
+
+
+
+function find_with_regular_expression(coll, nome)
+{
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => 
+    {
+      
+        if (err) throw err;
+    
+        const db = client.db("webdb");
+    
+        let collection = db.collection(coll);
+        let query = { name: nome }
+    
+        collection.findOne(query).then(doc => 
+        {
+            console.log(doc);
+        })
+        
+        .catch((err) => 
+        {
+            console.log(err);
+        })
+        
+        .finally(() => 
+        { 
+            client.close();
+        });
+    });
+  
+}
+
+
+
+
+
+
+
+//find_with_regular_expression("cars", /^A/); //trova tutti i nomi che iniziano per A
+//update_one("cars", "audi", 50);
+//remove_one("cars", "scemo");
+//add_one("test1", 10);
+//add_many();
+//find_one("cars", "scemo"); //return a document
+//find("cars", "scemo"); //return a collection
+// list_coll();
+
+
+
+/*
+
+Nuove Collezioni:
+
+
+Percorsi_>
+partenza da OLC e array di percorsi successivi
+
+
+
+
+*/
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,134 @@
+
+
+const express = require("express");
+const app = express();
+const port = 8000;
+const myModule = require('./mongo2.js');
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+
+app.use('/', express.static(__dirname + '/client'));
+app.use((req,res,next) => {
+    res.setHeader("Acces-Control-Allow-Origin", '*');
+    res.setHeader("Acces-Control-Allow-Headers", "Origin, X-Request-With, Content-Type, Accept");
+    res.setHeader("Acces-Control-Allow-Methods", "GET, POST");
+    next();
+});
+
+
+
+
+app.get("/categorie", function (req, res) { //richiesta del json con le categorie
+
+    console.log("requested: categorie.json");
+    res.sendFile(__dirname+"/client/categorie.json");
+
+});
+
+app.post("/new_place", async (req, res) => { // aggiunta di un nuovo luogo
+        
+    try {    
+        
+        let doc = await myModule.add_one(req);
+        res.send(doc);
+        
+    } catch (err) { //catch the error if the database isn't connected
+        res.send(err);
+    }
+});
+
+app.post("/new_review", async (req, res) =>{
+
+try {    
+        
+    let doc = await myModule.add_review(req);
+    res.send(doc);
+        
+    } catch (err) { //catch the error if the database isn't connected
+        res.send(err);
+    }
+
+})
+
+
+app.post("/find", async (req, res) => {
+   
+    try{
+
+        var doc = await myModule.find(req);
+        console.log(doc)
+        res.send(JSON.stringify(doc));
+    
+    }
+    catch(err){
+        res.send(err);
+    }
+
+});
+
+app.get("/list_place", async (req, res) => {
+    
+    try {
+
+    let ret = await myModule.showdb_place();
+    var resend = JSON.stringify(ret);
+    res.send(resend);
+    }
+    catch (err){
+        res.send("errore nella stampa della lista place")
+    }
+});
+
+app.get("/list_review", async (req, res) => {
+    
+    try {
+
+    let ret = await myModule.showdb_review();
+    res.send(JSON.stringify(ret));
+    }
+    catch (err){
+        res.send("errore nella stampa lista review")
+    }
+});
+
+app.get("/drop_place", async (req, res) => {
+    
+    try {
+
+    let ret = await myModule.clear_place();
+    res.send(JSON.stringify(ret));
+    }
+    catch (err){
+        res.send(err);
+    }
+});
+
+app.get("/drop_review", async (req, res) => {
+    
+    try {
+
+    let ret = await myModule.clear_review();
+    res.send(JSON.stringify(ret));
+    }
+    catch (err){
+        res.send(err);
+    }
+});
+
+app.post("/star", async (req, res) => {
+    
+    try{
+    let ret = await myModule.up_star(req);
+    res.send(JSON.stringify(ret));
+    
+}
+    catch (err){
+        res.send(err);
+    }
+
+
+});
+
+app.listen(port, () => console.log("Server started on port: " + port));
