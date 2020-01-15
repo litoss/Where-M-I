6fb862ce8310b713f88ae2fe9d8d99b7dd64306a
elpero <peropix@hotmail.it>
2020-01-15 16:54:23 +0100

    Classifiche paths

--- client/js/drawers/charts.js
+++ client/js/drawers/charts.js
@@ -20,7 +20,7 @@ async function openCharts(){
     }
   });
 
-  tabBar.activateTab(0);
+  tabBar.activateTab(2);
 
   content.appendChild(tabBar.root_);
   content.appendChild(div);
@@ -32,8 +32,44 @@ async function openCharts(){
 async function getTopPaths(div){
   var paths = new List("mdc-list--two-line");
 
-  for(var i in example) paths.add(new ElementList(example[i].primaryText, example[i].secondaryText, 'music_note'));
-  div.appendChild(paths.root_);
+  xhr = new XMLHttpRequest();
+  xhr.open('POST', '/find_route');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = async function(){
+    var response = JSON.parse(xhr.response);
+    var path = [];
+    for(var i in response){
+      var sum = 0;
+      for (var j in response[i].route){
+        place = await getRating(response[i].route[j]);
+        sum += place.media_rating;
+        if(j == 0) {
+          var image = decode64(place.image);
+        }
+      }
+
+      path[i] = {  route: response[i], rating: sum /response[i].route.length, image: image};
+    }
+
+    var comp = function(a, b){
+      return a.rating - b.rating;
+    }
+
+    path.sort(comp);
+    path.reverse();
+
+    var paths = new List("mdc-list--two-line mdc-list--avatar-list");
+
+    for(var i in path) paths.add(new ImageList(path[i].route.namer, "Media Rating: " + path[i].rating, path[i].image ));
+
+    paths.listen('MDCList:action', (event) => {
+      map.pageDrawer.open = false;
+      selectedPath(path[event.detail.index].route);
+    });
+
+    div.appendChild(paths.root_);
+  }
+  xhr.send(JSON.stringify({namer: ''}));
 }
 
 async function getTopVlogger(div){
@@ -94,4 +130,16 @@ async function getTopClips(div){
   });
 
   div.appendChild(listclips.root_);
+}
+
+function getRating(olc){
+  return new Promise((resolve,reject) =>{
+    var xhr = new XMLHttpRequest;
+    xhr.open('POST', '/find_place');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function(){
+      resolve(JSON.parse(xhr.response)[0]);
+    }
+    xhr.send(JSON.stringify({OLC: olc}));
+  })
 }
